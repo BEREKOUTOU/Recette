dab7479aea2f6181d5f4a4fc0564989f29149bcb
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-09-24 15:58:51 +0200

    fix: update viteSourceLocator prefix to match project structure

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   base: '/Recette/',
   plugins: [
     viteSourceLocator({
-      prefix: "mgx",
+      prefix: "Recette/src/",
     }),
     react(),
   ],
