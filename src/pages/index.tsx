f2fb66960cee7ba25c67e6f985e0e4e2ac285cee
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-09-24 16:00:30 +0200

    `Update layout of Index page to add flexbox styling`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 
 export default function Index() {
   return (
-    <div className="p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-4xl font-bold">Recherche de Recettes</h1>
       <p className="mt-4">Trouvez des recettes par ingrédients, restrictions alimentaires et temps de cuisson.</p>
     </div>
