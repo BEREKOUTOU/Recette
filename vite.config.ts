db9e439aece424f3dfd980e4549fbde7e9816042
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-09-25 10:28:41 +0200

    `Removed unused imports and plugins from vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
+// import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Recette/',
+  // base: '/Recette/',
   plugins: [
-    viteSourceLocator({
-      prefix: "Recette/src/",
-    }),
+    // viteSourceLocator({
+    //   prefix: "Recette/src/",
+    // }),
     react(),
   ],
   resolve: {
