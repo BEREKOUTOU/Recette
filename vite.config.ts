0f2910bfb2ee524ee50d1df91f0c84dc4ee8189f
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-09-25 10:32:27 +0200

    fix: uncomment base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/Recette/',
+  base: '/Recette/',
   plugins: [
     // viteSourceLocator({
     //   prefix: "Recette/src/",
